2f442c784a836996dc5c0b03ef168145dd09d792
Yuxuan Song <3075515s@student.gla.ac.uk>
2026-02-12 19:44:30 +0000

    Chapter 10 completed

--- rango/views.py
+++ rango/views.py
@@ -9,20 +9,31 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def about(request):
-    context_dict2={'boldmessage':'Rango says here is the about page.'}
-    return render(request,'rango/about.html',context=context_dict2)
+    visitor_cookie_handler(request)
+    
+    context_dict = {'boldmessage': 'Rango says here is the about page.'}
+    context_dict['visits'] = request.session.get('visits', 0)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    return render(request, 'rango/about.html', context=context_dict)
+
 def index(request):
-# Construct a dictionary to pass to the template engine as its context.
-    category_list=Category.objects.order_by('-likes')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict={}
-    context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories']=category_list
+
+    context_dict = {}
+    context_dict['boldmessage'] ='Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-# Return a rendered response to send to the client.
-    return render(request, 'rango/index.html', context=context_dict)
+
+    visitor_cookie_handler(request)
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
 # Create a context dictionary which we can pass
@@ -130,5 +141,33 @@ def user_logout(request):
     logout(request)
 # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# rango/views.py
+
+def visitor_cookie_handler(request):
+    visits = int(request.session.get('visits', '1'))
+    
+    last_visit_cookie = request.session.get('last_visit')
+    if last_visit_cookie:
+        try:
+            last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+            
+            if (datetime.now() - last_visit_time).days > 0:
+                visits = visits + 1
+                request.session['last_visit'] = str(datetime.now())
+            else:
+                request.session['last_visit'] = last_visit_cookie
+        except ValueError:
+            request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = str(datetime.now())
+        visits = 1
+    request.session['visits'] = visits
     
 
